460d9e7003c80a0e1dfd8e47b19687843936c30d
kecebongsoft <kecebongsoft@gmail.com>
2011-07-18 18:00:08 +0800

    Added paginator

--- apps/home/views.py
+++ apps/home/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.core.paginator import Paginator, InvalidPage, EmptyPage
 
 from posts.models import Post
 from tags.models import Tag
@@ -8,8 +9,21 @@ from tags.models import Tag
 #Home is just a home, its just a single page that combines everything
 #Please separate another page into a single app instead
 def home(request):
+    posts_latests = Post.get_latests()
+    paginator = Paginator(posts_latests, 10)
+    try:
+        page = int(request.GET.get('page','1'))
+        posts = paginator.page(page)
+    except ValueError:
+        page = 1
+    
+    try:
+        posts = paginator.page(page)
+    except (EmptyPage,InvalidPage):
+        posts = paginator.page(paginator.num_pages)
+
     context = {
-                'posts_latests' : Post.get_latests(),
+                'posts_latests' : posts,
                 'tag_list' : Tag.tree.all()
               }
 
--- apps/tags/views.py
+++ apps/tags/views.py
@@ -1,15 +1,29 @@
 # Create your views here.
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
+from django.core.paginator import Paginator, InvalidPage, EmptyPage
 from .models import Tag,TagPost
-
+from posts.models import Post
 def tag_post_list(request, tag_id, slug):
     tag = get_object_or_404(Tag, pk = tag_id)
-    tag_post = TagPost.objects.filter(tag = tag).order_by('-post__date_created')
+    tag_post = TagPost.objects.filter(tag = tag).order_by('-post__date_sorted')
     tag_post.query.group_by = ['post_id']
-    post = [tp.post for tp in tag_post]
+    post_list = [tp.post for tp in tag_post]
+    paginator = Paginator(post_list, 10)
+    try:
+        page = int(request.GET.get('page','1'))
+        posts = paginator.page(page)
+    except ValueError:
+        page = 1
+    
+    try:
+        posts = paginator.page(page)
+    except (EmptyPage,InvalidPage):
+        posts = paginator.page(paginator.num_pages)
+
+
     context = {
-                'post' : post,
+                'posts' : posts,
               }
 
     return render_to_response(
