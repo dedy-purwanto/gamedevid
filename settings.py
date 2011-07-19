b4335355e2f2675d15103f1b624793ba9facd978
kecebongsoft <kecebongsoft@gmail.com>
2011-07-19 18:20:31 +0800

    Added image url in tinymce

--- settings.py
+++ settings.py
@@ -145,7 +145,7 @@ TINYMCE_DEFAULT_CONFIG = {
     'plugins' : "autolink,lists,spellchecker,pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,nonbreaking,xhtmlxtras,template",
 
     #// Theme options
-    'theme_advanced_buttons1' : "bold,italic,underline,strikethrough,fontsizeselect,|,justifyleft,justifycenter,justifyright,justifyfull,|fontsizeselect,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchorcode,|,`insertdate,inserttime,preview,|,forecolor,backcolor",
+    'theme_advanced_buttons1' : "bold,italic,underline,strikethrough,fontsizeselect,|,justifyleft,justifycenter,justifyright,justifyfull,|fontsizeselect,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,image,|,`insertdate,inserttime,preview,|,forecolor,backcolor",
     'theme_advanced_buttons2' : "",
     'theme_advanced_buttons3' : "",
     'theme_advanced_toolbar_location' : "top",
