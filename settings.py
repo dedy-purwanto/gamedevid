df1c05b2cfaad7047857d5da4201ebcd69e9a886
kecebongsoft <kecebongsoft@gmail.com>
2011-07-21 18:29:13 +0800

    Add youtube embed in post editor

--- settings.py
+++ settings.py
@@ -142,10 +142,10 @@ LOGIN_URL = '/users/login/'
 TINYMCE_DEFAULT_CONFIG = {
     'mode' : "textareas",
     'theme' : "advanced",
-    'plugins' : "autolink,lists,spellchecker,pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,nonbreaking,xhtmlxtras,template",
+    'plugins' : "autolink,lists,spellchecker,pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,nonbreaking,xhtmlxtras,template,media",
 
     #// Theme options
-    'theme_advanced_buttons1' : "bold,italic,underline,strikethrough,fontsizeselect,|,justifyleft,justifycenter,justifyright,justifyfull,|fontsizeselect,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,image,|,`insertdate,inserttime,preview,|,forecolor,backcolor",
+    'theme_advanced_buttons1' : "bold,italic,underline,strikethrough,fontsizeselect,|,justifyleft,justifycenter,justifyright,justifyfull,|fontsizeselect,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,image,media,|,`insertdate,inserttime,preview,|,forecolor,backcolor",
     'theme_advanced_buttons2' : "",
     'theme_advanced_buttons3' : "",
     'theme_advanced_toolbar_location' : "top",
